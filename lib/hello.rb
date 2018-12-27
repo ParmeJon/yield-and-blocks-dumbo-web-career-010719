65b912f959a0963f66fd3ac92ff0c5f45cbf95d1
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 09:52:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,11 @@
 def hello_t(arr)
-  i = 0 
-  
-  while i < arr.length 
-    yield (arr[i])
-    i = i + 1 
-  end
+  if block_given?
+    i = 0 
+    
+    while i < arr.length 
+      yield (arr[i])
+      i = i + 1 
+    end
   
   return arr
 end
